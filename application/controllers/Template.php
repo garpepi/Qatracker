6e0b473b159332ae4a8e834c45f6f33add4885fc
Garpepi H Aotearoa <garpepi.aotearoa@gmail.com>
2016-08-10 16:36:16 +0700

    Remove file to change file name upcase

--- application/controllers/Template.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    class Template extends MY_Controller {
-        public function index() {
-			
-            $this->contents = 'template'; // its your view name, change for as per requirement.
-            $this->layout();
-        }
-    }
