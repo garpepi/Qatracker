854be3aa086cee67d9138a96228ba9d13d31da20
Garpepi Hanief Aotearoa <garpepi.aotearoa@gmail.com>
2016-09-05 13:34:36 +0700

    Fixing Name and adding logout

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -13,7 +13,7 @@
 	  </div>
 	  <div class="profile_info">
 		<span>Welcome,</span>
-		<h2>John Doe</h2>
+		<h2><?php echo $this->session->userdata('logged_in_data')['name']; ?></h2>
 	  </div>
 	</div>
 	<!-- /menu profile quick info -->
