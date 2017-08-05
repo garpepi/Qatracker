1eaa48a5d7adb16fc26c124c4b10a83c498dbe01
Garpepi H Aotearoa <garpepi.aotearoa@gmail.com>
2017-08-05 17:11:53 +0700

    Correction auto email report

--- application/controllers/Autoreport.php
+++ application/controllers/Autoreport.php
@@ -1,11 +1,11 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    class Autoreport extends CI_Controller {		
+    class Autoreport extends CI_Controller {
 		public function __construct() {
 		   parent::__construct();
-		   $this->load->model('projects_model');	
+		   $this->load->model('projects_model');
 			$this->load->model('daily_reports_model');
 		}
-		
+
 		private function generate_report($data_gen, $date){
 			$header[] = array(
 						'Timestamp',
@@ -38,13 +38,13 @@
 			$this->load->library('excel');
 			$this->excel->setActiveSheetIndex(0);
 			$this->excel->getActiveSheet()->setTitle('Adidata QA Daily Report'); // naming sheet
-			
-			
+
+
 			$filename='Summary Report QA '.$date.'.xls'; //save our workbook as this file name
 		//	header('Content-Type: application/vnd.ms-excel'); //mime type
 		//	header('Content-Disposition: attachment;filename="'.$filename.'"'); //tell browser what's the file name
 		//	header('Cache-Control: max-age=0'); //no cache
-			$this->excel->getActiveSheet()->fromArray( 
+			$this->excel->getActiveSheet()->fromArray(
 					$data,  // The data to set
 					NULL,        // Array values with this value will not be set
 					'A1'         // Top left coordinate of the worksheet range where
@@ -58,11 +58,11 @@
 			}
 			//save it to Excel5 format (excel 2003 .XLS file), change this to 'Excel2007' (and adjust the filename extension, also the header mime type)
 			//if you want to save it as .XLSX Excel 2007 format
-			$objWriter = PHPExcel_IOFactory::createWriter($this->excel, 'Excel5');  
+			$objWriter = PHPExcel_IOFactory::createWriter($this->excel, 'Excel5');
 			//force user to download the Excel file without writing it to server's HD
 			$objWriter->save('./genreports/'.$filename);
 		}
-		
+
 		public function autogenerate(){
 			//validate if not 6 a clock
 			/*
@@ -74,15 +74,15 @@
 			if(!is_cli()){
 				exit();
 			}
-			$where = array();			
-			
+			$where = array();
+
 			if(date('j') == 1){
 				$where += array('daily_reports.created_date >= ' => date('Y-m',strtotime('-1 month')).'-01 00:00:00');
-				$where += array('daily_reports.created_date < ' => date('Y-m-d 00:00:00')); 
+				$where += array('daily_reports.created_date < ' => date('Y-m-d 00:00:00'));
 			}else{
 				$where += array('daily_reports.created_date >= ' => date('Y-m').'-01 00:00:00');
-				$where += array('daily_reports.created_date < ' => date('Y-m-d 00:00:00')); 
-			}				
+				$where += array('daily_reports.created_date < ' => date('Y-m-d 00:00:00'));
+			}
 			$fetch = $this->daily_reports_model->get_reports($where,'tester_name asc, daily_reports.created_date asc');
 			$data = array();
 			// Write log generate to file
@@ -100,7 +100,7 @@
 						$application_list .= $app_data['name'];
 					}
 				}
-				
+
 				// Time Convert
 				//
 				$d = floor ($value['downtimes'] / 1440);
@@ -131,50 +131,50 @@
 						(!empty($value['project']['plan_start_doc_date']) ? date('Y-m-d',strtotime($value['project']['plan_start_doc_date']) ):'' ),
 						(!empty($value['project']['plan_end_doc_date']) ? date('Y-m-d',strtotime($value['project']['plan_end_doc_date']) ):'' ),
 						(!empty($value['project']['plan_end_doc_date']) ? date('Y-m-d',strtotime($value['project']['plan_end_doc_date']) ):'' ),
-						(!empty($value['actual_end_doc_date']) ? date('Y-m-d',strtotime($value['actual_end_doc_date']) ):'' )			
+						(!empty($value['actual_end_doc_date']) ? date('Y-m-d',strtotime($value['actual_end_doc_date']) ):'' )
 						);
-				
+
 				array_push($data,$temp);
 			}
-			
-			$this->generate_report($data,date('d M Y'));			
-			
+
+			$this->generate_report($data,date('d M Y'));
+
 		}
-		
-		private function sending_email($from_email, $from_name, $to, $cc= array(), $subject, $message, $attachment = ''){			
+
+		private function sending_email($from_email, $from_name, $to, $cc= array(), $subject, $message, $attachment = ''){
 			$this->load->library('email');
 		//$this->email->initialize($config);
 			$this->email->from($from_email, $from_name);
 			$this->email->to($to);
 			if(!empty($cc)){
-				$this->email->cc($cc);				
+				$this->email->cc($cc);
 			}
-			
+
 			$this->email->subject($subject);
 			$this->email->message($message);
 			if(!empty($attachment)){
 				if(is_file($attachment)){
-					$this->email->attach($attachment);									
+					$this->email->attach($attachment);
 				}else{
 					echo 'file not found - '. $attachment;
 					exit();
 				}
 			}
-			
+
 			if($this->email->send()){
 				return 'Sending Ok';
 			}else{
 				return 'Sending Not Ok';
 			}
-			
+
 		}
-		
+
 		public function autoemail(){
 			//validate if not 9 a clock
 			/*if(date('H') != 9){
 				exit();
 			}*/
-			
+
 			//if cli
 			if(!is_cli()){
 				exit();
@@ -182,7 +182,7 @@
 			$this->config->load('qa_tracker_config');
 			$this->load->model('api_model');
 			$milist = $this->api_model->get_milist()['data'];
-			
+
 			foreach($milist as $value)
 			{
 				$email[]=$value->email;
@@ -194,24 +194,34 @@
 			}else{
 				$date = date('Y-m').'-01 00:00:00'.' - '.date('Y-m-d H:i:s');
 			}
-			
+
 			$this->load->model('api_model');
-			
+
 			$holiday_date = array();
 			foreach($this->api_model->get_holiday_list()['data'] as $holiday)
 			{
 				$holiday_date[] = $holiday->date;
 			}
-			
+
 			if(in_array(date('Y-m-d'), $holiday_date))
 			{
 				return 'Today is holiday';
 			}else{
-				return $this->sending_email( $this->config->item('email_sender'), 'Qatracker App', $email, array(), 'Qa Tracker Report '.date('d M Y'), "Dear Team BM, \n\n Berikut ini kami sampaikan daily report dari Team Adidata (terlampir). \n\n Thanks & Regards, \n Helpdesk@Adidata.co.id \n\n\n\n Qatracker App - by Garpepi", './genreports/Summary Report QA '.date('d M Y').'.xls');				
+        if(!$this->isWeekend(date('Y-m-d')))
+        {
+          return $this->sending_email( $this->config->item('email_sender'), 'Qatracker App', $email, array(), 'Qa Tracker Report '.date('d M Y'), "Dear Team BM, \n\n Berikut ini kami sampaikan daily report dari Team Adidata (terlampir). \n\n Thanks & Regards, \n Helpdesk@Adidata.co.id \n\n\n\n Qatracker App - by Garpepi", './genreports/Summary Report QA '.date('d M Y').'.xls');
+        }else{
+          return 'Today is weekend';
+        }
+
 			}
-		
+
 		}
-		
+
+    public function isWeekend($date) {
+        return (date('N', strtotime($date)) >= 6);
+    }
+
 		public function test()
 		{
 			$this->load->model('api_model');
@@ -220,8 +230,8 @@
 			foreach($this->api_model->get_milist()['data'] as $milists)
 			{
 				$milist[] = $milists->email;
-			}			
-			
+			}
+
 		}
 
     }
