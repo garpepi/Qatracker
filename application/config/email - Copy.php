ecb7e594994335b148a163101c6d487b2d5d1290
GHA - HP <garpepi.aotearaoa@gmail.com>
2019-07-28 18:23:15 +0700

    adding gitignore adding email config example

---
+++ application/config/email - Copy.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'jkt20.dewaweb.com';
+$config['smtp_user'] = 'no-reply.hrm@adi-internal.com';
+$config['smtp_pass'] = 'hrmadidata2019';
+$config['smtp_port'] = 465; 
+$config['smtp_crypto'] = 'ssl'; 
